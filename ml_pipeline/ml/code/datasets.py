ba24f85415f6d28541ca5d52df1349701316fd12
yemilyz <ezhao@g.hmc.edu>
2019-11-07 10:50:01 -0800

    Removed files that do not pertain to clinic project

--- ml_pipeline/ml/code/datasets.py
+++ ml_pipeline/ml/code/datasets.py
@@ -15,57 +15,6 @@ import pandas as pd
 ######################################################################
 # functions
 ######################################################################
-
-def iris():
-    """Load iris dataset"""
-
-    # mapping of species to integers
-    target_names = ["setosa", "versicolor", "virginica"]  # display names of labels
-    labels = [0, 1, 2]                                    # labels as indices
-    species_map = dict(zip(map(lambda name: "Iris-%s" % name, target_names),
-                            labels))
-
-    # read csv
-    df = pd.read_csv("data/iris.csv")
-
-    # process
-    df["Species"] = df["Species"].map(species_map) # map labels
-    df = df.drop("Id", axis=1)      # drop Id column
-
-    # get features, labels, and feature names
-    X = df.drop("Species", axis=1)
-    y = df["Species"]
-    feature_names = X.columns
-
-    return X, y, labels, target_names, feature_names
-
-
-
-def titanic():
-    """Load titanic dataset"""
-
-    target_names = ["No", "Yes"]
-    labels = [0, 1]
-
-    # read csv
-    df = pd.read_csv("data/titanic.csv")
-
-    # part b: process
-    df = df.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1)
-    df = df.replace({'female': 0, 'male': 1})
-    df = pd.concat((df[['Embarked']],
-          pd.get_dummies(df, columns=['Embarked'], drop_first=True)),
-          axis=1)
-    df = df.drop(['Embarked'], axis=1)
-    df = df.dropna(subset=['Survived'])
-    # get features, labels, and feature_names
-    X = df.drop("Survived", axis=1)
-    y = df["Survived"]
-    feature_names = X.columns
-
-    return X, y, labels, target_names, feature_names
-
-
 def DI():
     """Load DI dataset"""
 
@@ -76,15 +25,15 @@ def DI():
     df = pd.read_csv("data/labeled_data_modified_test.csv")
 
     # part b: process
-    df = df.drop(['Name','Developability Index (All)'], axis=1)
+    df = df.drop(['Name', 'DI Classification'], axis=1)
     # df = df.replace({'female': 0, 'male': 1})
     # df = pd.concat((df[['Embarked']],
     #       pd.get_dummies(df, columns=['Embarked'], drop_first=True)),
     #       axis=1)
     # df = df.drop(['Embarked'], axis=1)
     # get features, labels, and feature_names
-    X = df.drop("DI Classification", axis=1)
-    y = df["DI Classification"]
+    X = df.drop("Developability Index (All)", axis=1)
+    y = df["Developability Index (All)"]
     feature_names = X.columns
 
     return X, y, labels, target_names, feature_names
