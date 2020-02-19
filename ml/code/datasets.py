fe5b6589b83cc3da136325e2af3b4e3f93b6ccd4
yemilyz <ezhao@g.hmc.edu>
2020-02-19 14:25:51 -0800

    dev delete

--- ml/code/datasets.py
+++ ml/code/datasets.py
@@ -46,6 +46,14 @@ def get_dataset(feature_path, label_path=DI_LABELS_CSV):
             del X['pdb_code']
         except KeyError:
             pass
+    try:
+        del X['Dev']
+    except:
+        pass
+    try:
+        del X['DI_all']
+    except:
+        pass
     X = X.loc[y.Name]
     feature_names = list(X)
     y = y['Developability Index (Fv)'] >= y['Developability Index (Fv)'].describe(percentiles=[0.75])[5]
